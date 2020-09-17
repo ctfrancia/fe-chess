4a91041adfa671b5883a2daabef6e8712666170b
Christian Francia <ctfrancia@gmail.com>
2020-09-17 21:58:02 +0200

    add: basic scaffolding to Tournament vue

--- src/views/Tournament.vue
+++ src/views/Tournament.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-  {{ tournament }} 
+  <div id="component-container">
+    <div class="tournament-header">
+      {{ tournament.Title }}
+    </div>
+
+    <div class="tournament-location">
+      {{ tournament.Location }}
+    </div>
+    
+    <div class="tournament-meta-data">
+      Online: {{ tournament.IsOnline }}
+      Rated: {{ tournament.Rated }}
+      Time start:{{ tournament.MatchTimeStart }}
+      TournamentType: {{ tournament.TournamentType }}
+      Time end: {{ tournament.MatchTimeEnd }}
+      Time control: {{ tournament.TimeControl }}
+      Date: {{ tournament.TournamentDate }} 
+      Contact: {{ tournament.TournamentContact }}
+      Date Created: {{ tournament.Created }}
+    </div>
+
+    <div class="tournament-poster">
+      poster goes here
+    </div>
+
+    <div class="tournament-additional-information">
+      {{ tournament.AdditionalInformation }} 
+    </div>
+
+    <div>
+    </div>
+  <!-- {{ tournament }} -->
   </div>
 </template>
 
@@ -20,4 +50,10 @@ export default {
 </script>
 
 <style scoped>
+  #component-container {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid blue;
+    border-radius: 10px;
+  }
 </style>
